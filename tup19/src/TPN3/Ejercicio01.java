057e917469aca1fced963678369e38d234dbe3bf
jeremiascardoso00 <53924082+jeremiascardoso00@users.noreply.github.com>
2019-08-16 00:32:49 -0300

    Add files via upload

---
+++ tup19/src/TPN3/Ejercicio01.java
@@ -0,0 +1,60 @@
+package TPN3;
+
+import java.util.Arrays;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		/*Ejercicio 01:
+		Crear un array de 100 elementos que guarde en cada una de las posiciones un número aleatorio entre 1 y 100.
+		El programa debe imprimir en consola todos los valores almacenados en el array.
+		Utiliza un bucle for-each para leer los valores almacenados.
+		(Debes utilizar el método random() de la clase Math para poder rellenar los elementos del array.
+		BONUS: Verificar que el número no se repita.*/
+
+        int array [] = new int [100];
+        int copia[] = new int [100];
+        int c=0;
+
+        for (int x=0;x<array.length;x++ ) {
+            array[x]= (int) (Math.random() * 100);
+
+        }
+        System.out.println("arreglo desordenado con numeros repetidos: ");
+        for(int fila:array) {
+
+            System.out.print(fila+" ");
+
+        }
+
+        for (int j=0;j<array.length;j++){
+            for (int x=0; x<array.length-1;x++){
+                for (int y=x+1; y<array.length;y++){
+                    if(array[x] == array[y]){
+                        array[y]++;
+                        if (array[y]>=100){
+                            array[y] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println();
+        System.out.println("arreglo desordenado sin ningun numero repetido: ");
+        for(int fila:array) {
+
+            System.out.print(fila+" ");
+
+        }
+        Arrays.sort(array);
+        System.out.println();
+        System.out.println("arreglo ordenado sin ningun numero repetido: ");
+        for(int fila:array) {
+
+            System.out.print(fila+" ");
+
+        }
+	}
+
+}
---
+++ tup19/src/TPN3/Ejercicio02.java
@@ -0,0 +1,10 @@
+package TPN3;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
