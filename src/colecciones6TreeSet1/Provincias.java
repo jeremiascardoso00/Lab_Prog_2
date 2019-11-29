4b38ffd33d2af85ae1249d7e960aa99fdbbf6e7c
jeremiascardoso00 <53924082+jeremiascardoso00@users.noreply.github.com>
2019-11-29 10:52:30 -0300

    subiendo practica

---
+++ src/colecciones6TreeSet1/Provincias.java
@@ -0,0 +1,24 @@
+package colecciones6TreeSet1;
+
+import java.util.ListIterator;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Provincias {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TreeSet<String> provincias = new TreeSet<String>();
+
+		provincias.add("Misiones");
+		provincias.add("Chaco");
+		provincias.add("Corrientes");		
+		provincias.add("Buenos Aires");
+	
+		
+		System.out.println(provincias);
+
+	}
+
+}
